c90fb8d35bc75455bf32f2469c546c20a494a3d4
Lityuk <lityuk@gmail.com>
2022-12-06 00:29:39 +0300

    start project again

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Todolist} from "./Todolist";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const task1 = [
+        {id: 1, title: "HTML&CSS", isDone: true},
+        {id: 2, title: "JS", isDone: true},
+        {id: 3, title: "ReactJS", isDone: false},
+        {id: 4, title: "Rest Api", isDone: true},
+        {id: 5, title: "Angular", isDone: false},
+    ]
+
+    return (
+        <div className="App">
+            <Todolist title="What to learn" tasks={task1}/>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Todolist.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+type TodolistPropsType = {
+    title: string
+    tasks: Array<TasksType>
+}
+
+type TasksType = {
+    id: number
+    title: string,
+    isDone: boolean
+}
+
+
+export const Todolist = (props: TodolistPropsType) => {
+
+    return (
+        <div>
+            <h3> {props.title}</h3>
+            <div>
+                <input/>
+                <button>+</button>
+            </div>
+            <ul>
+                {props.tasks.map(t => (
+                    <li><input key={t.id} type="checkbox" checked={t.isDone}/> <span>{t.title}</span></li>))}
+            </ul>
+            <div>
+                <button>All</button>
+                <button>Active</button>
+                <button>Completed</button>
+            </div>
+        </div>
+    )
+
+
+}
